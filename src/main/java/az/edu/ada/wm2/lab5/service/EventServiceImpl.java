4b3c91bec0e5035b1cb100f21599cee3331dbb4e
jala-asadli <jasadli16143@ada.edu.az>
2026-02-26 10:47:48 +0400

    Add EventServiceImpl:updateEventPrice implementation

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -149,7 +149,17 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Event updateEventPrice(UUID id, BigDecimal newPrice) {
-        return null;
-    }
+        if (id == null || newPrice == null || newPrice.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("invalid id or price");
+        }
 
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Event not found with id: " + id));
+
+        event.setTicketPrice(newPrice);
+
+        return eventRepository.save(event);
+    }
 }
+
+
